7877e9b4c9be3aa6a6428d6071268f7f1b15238f
Vinicius Rosendo <viniciusrosendo17@gmail.com>
2024-05-21 02:22:42 -0300

    projeto conta terminal

--- CONTA-BANCO/src/BancoTerminal.java
+++ CONTA-BANCO/src/BancoTerminal.java
@@ -1,5 +1,5 @@
 public class BancoTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Testando primeiro commit");
     }
 }
